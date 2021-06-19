ee704888a913ebd9ba75e65233862830e4a8066e
Mohammed Hany <47370413+Mohammed-Hani@users.noreply.github.com>
2021-06-20 01:31:08 +0200

    feat: Add health status check api endpoint Add health check api endpoint to      - return an HTTP 200 status code      - return a json with the app status

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def status_check():
+    return jsonify({
+            'result': 'OK - healthy'
+            })
+
+@app.route("/metrics")
+def metrics_check():
+    return jsonify({
+            'success': True,
+            
+            })
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
