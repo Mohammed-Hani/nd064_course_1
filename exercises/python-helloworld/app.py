d33e8c7c81a7e3d460df29b3ff2b973fb31dd9dd
Mohammed Hany <47370413+Mohammed-Hani@users.noreply.github.com>
2021-06-20 02:58:41 +0200

    feat: Add logging to the app

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,11 +1,26 @@
 from flask import Flask, jsonify
+import logging
+from functools import wraps
+
+
+logging.basicConfig(filename='app.log', format='%(asctime)s, %(message)s', level=logging.INFO)
 app = Flask(__name__)
 
+def endpoint_reached(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        logging.info(func.__name__ + ' endpoint was reached')
+        return func(*args, **kwargs)
+    return wrapper
+
+
 @app.route("/")
+@endpoint_reached
 def hello():
     return "Hello World!"
 
 @app.route("/status")
+@endpoint_reached
 def status_check():
     return jsonify({
             'success':True,
@@ -13,6 +28,7 @@ def status_check():
             })
 
 @app.route("/metrics")
+@endpoint_reached
 def metrics_check():
     metrics_data = {'UserCount': 140, 'UserCountActive': 23}
     return jsonify({
